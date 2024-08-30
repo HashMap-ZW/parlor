5e381d1f52bfeac2acf4221aaa38cc7aa6610b83
Tello <pateltanaka22@live.com>
2024-08-30 23:59:03 +0200

    adjusted the dashboard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,7 +13,7 @@ export const routes: Routes = [
     component: LayoutComponent,
     children: [
       {
-        path: '',
+        path: 'dashboard',
         component: DashboardComponent,
       },
     ],
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -4,25 +4,25 @@ import { Component, type OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent implements OnInit {
-
-  routerLinks =[
+  routerLinks = [
     {
-      name:'Dashboard',
-      link:' '
+      name: 'Dashboard',
+      link: ' ',
     },
     {
-      name:'Users',
-      link:'users'
-    }
-  ]
+      name: 'Payments',
+      link: 'payments',
+    },
+    {
+      name: 'Users',
+      link: 'users',
+    },
+  ];
 
-  ngOnInit(): void { }
-
+  ngOnInit(): void {}
 }
