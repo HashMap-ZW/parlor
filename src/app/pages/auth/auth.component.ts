b452f9fc7803785d17376334e2a6a2c0ee7292e3
Tello <pateltanaka22@live.com>
2024-08-30 23:37:15 +0200

    go to  dashboard when user logs in

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,17 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component, type OnInit } from '@angular/core';
+import { Component, inject, type OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../tools/services';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
   imports: [
     CommonModule,
+    RouterModule,
+    ReactiveFormsModule
   ],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss',
 })
 export class AuthComponent implements OnInit {
 
+  authService = inject(AuthService);
+
   ngOnInit(): void { }
+
+  submit(){
+    this.authService.goToRoute('dashboard');
+  }
 
 }
