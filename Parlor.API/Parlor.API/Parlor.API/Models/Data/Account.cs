a5b54f091829d6e69c9a9e4828af5bd82aa73bc5
Keith Stanley <iamstan13y@gmail.com>
2024-08-31 04:04:11 +0200

    A Few Necessary Configs & Stuff.

---
+++ Parlor.API/Parlor.API/Parlor.API/Models/Data/Account.cs
@@ -0,0 +1,20 @@
+﻿using Parlor.API.Models.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Parlor.API.Models.Data;
+
+public class Account
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? UserName { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Password { get; set; }
+    public bool IsActive { get; set; } = false;
+    public Role Role { get; set; } = Role.User;
+    public DateTime DateCreated { get; set; } = DateTime.Now;
+    [NotMapped]
+    public string? Token { get; set; }
+}
