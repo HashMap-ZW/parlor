611a8b5803cf378e92b0dc2a3f7643741adb71eb
Keith Stanley <iamstan13y@gmail.com>
2024-08-31 04:04:35 +0200

    A Few Necessary Configs & Stuff.

---
+++ Parlor.API/Parlor.API/Parlor.API/Models/Repository/AccountRepository.cs
@@ -0,0 +1,42 @@
+﻿using Parlor.API.Models.Data;
+using Parlor.API.Models.Local;
+using Parlor.API.Models.Repository.IRepository;
+
+namespace Parlor.API.Models.Repository;
+
+public class AccountRepository : IAccountRepository
+{
+    //public Task<Result<Account>> ChangePasswordAsync(ChangePasswordRequest changePasswordRequest)
+    //{
+    //    throw new NotImplementedException();
+    //}
+
+    //public Task<Result<Account>> ConfirmAccountAsync(ConfirmAccountRequest confirmAccountRequest)
+    //{
+    //    throw new NotImplementedException();
+    //}
+
+    //public Task<Result<string>> GetResetPasswordCodeAsync(string email)
+    //{
+    //    throw new NotImplementedException();
+    //}
+
+    //public Task<Result<Account>> LoginAsync(LoginRequest request)
+    //{
+    //    throw new NotImplementedException();
+    //}
+
+    //public Task<Result<string>> ResendOtpAsync(string email)
+    //{
+    //    throw new NotImplementedException();
+    //}
+
+    //public Task<Result<Account>> ResetPasswordAsync(ResetPasswordRequest resetPasswordRequest)
+    //{
+    //    throw new NotImplementedException();
+    //}
+    public Task<Result<Account>> AddAsync(Account account)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Parlor.API/Parlor.API/Parlor.API/Models/Repository/IRepository/IAccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Parlor.API.Models.Data;
+using Parlor.API.Models.Local;
+
+namespace Parlor.API.Models.Repository.IRepository;
+
+public interface IAccountRepository
+{
+
+    Task<Result<Account>> AddAsync(Account account);
+    //Task<Result<Account>> LoginAsync(LoginRequest request);
+    //Task<Result<Account>> ChangePasswordAsync(ChangePasswordRequest changePasswordRequest);
+    //Task<Result<string>> GetResetPasswordCodeAsync(string email);
+    //Task<Result<Account>> ResetPasswordAsync(ResetPasswordRequest resetPasswordRequest);
+    //Task<Result<Account>> ConfirmAccountAsync(ConfirmAccountRequest confirmAccountRequest);
+    //Task<Result<string>> ResendOtpAsync(string email);
+}
