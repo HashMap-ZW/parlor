6e0e3c491504e6cd6505eff05d7e72e679fd1125
Keith Stanley <iamstan13y@gmail.com>
2024-09-01 19:20:49 +0200

    Creaate Account Implementation.

---
+++ Parlor.API/Parlor.API/Parlor.API/Services/OtpService.cs
@@ -0,0 +1,24 @@
+﻿using Parlor.API.Services.IServices;
+using System.Text;
+
+namespace Parlor.API.Services
+{
+    public class OtpService(IConfiguration configuration) : IOtpService
+    {
+        private readonly IConfiguration _configuration = configuration;
+        private readonly Random _rnd = new();
+
+        public Task<string> GenerateOtpCode()
+        {
+            string chars = _configuration["OtpService:Chars"]!;
+            var code = new StringBuilder(6);
+
+            for (int i = 0; i < 6; i++)
+            {
+                code.Append(chars[_rnd.Next(chars.Length)]);
+            }
+
+            return Task.FromResult(code.ToString());
+        }
+    }
+}
