1d3969e4e21b0fd01fde03beb71d668ffa872422
Keith Stanley <iamstan13y@gmail.com>
2024-09-01 19:20:35 +0200

    Creaate Account Implementation.

--- Parlor.API/Parlor.API/Parlor.API/Models/Repository/AccountRepository.cs
+++ Parlor.API/Parlor.API/Parlor.API/Models/Repository/AccountRepository.cs
@@ -1,11 +1,20 @@
-﻿using Parlor.API.Models.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Parlor.API.Models.Data;
 using Parlor.API.Models.Local;
 using Parlor.API.Models.Repository.IRepository;
+using Parlor.API.Services.IServices;
 
 namespace Parlor.API.Models.Repository;
 
-public class AccountRepository : IAccountRepository
+public class AccountRepository(AppDbContext context, IConfiguration configuration, IPasswordService passwordService, IJwtService jwtService, IOtpService otpService, IEmailService emailService) : IAccountRepository
 {
+    private readonly AppDbContext _context = context;
+    private readonly IConfiguration _configuration = configuration;
+    private readonly IPasswordService _passwordService = passwordService;
+    private readonly IJwtService _jwtService = jwtService;
+    private readonly IOtpService _otpService = otpService;
+    private readonly IEmailService _emailService = emailService;
+
     //public Task<Result<Account>> ChangePasswordAsync(ChangePasswordRequest changePasswordRequest)
     //{
     //    throw new NotImplementedException();
@@ -35,8 +44,48 @@ public class AccountRepository : IAccountRepository
     //{
     //    throw new NotImplementedException();
     //}
-    public Task<Result<Account>> AddAsync(Account account)
+    public async Task<Result<Account>> AddAsync(Account account)
     {
-        throw new NotImplementedException();
+        try
+        {
+            if (!IsUniqueUser(account.Email!))
+                return new Result<Account>(false, "An account with that email already exists!");
+
+            account.Password = _passwordService.HashPassword(account.Password!);
+
+            await _context.Accounts!.AddAsync(account);
+
+            var code = await _otpService.GenerateOtpCode();
+
+            await _context.OtpCodes!.AddAsync(new OtpCode
+            {
+                Code = code,
+                UserEmail = account.Email,
+                DateCreated = DateTime.Now
+            });
+
+            //await _emailService.SendEmailAsync(new EmailRequest
+            //{
+            //    To = account.Email,
+            //    Subject = _configuration["EmailService:ConfirmAccountSubject"],
+            //    Body = string.Format(_configuration["EmailService:ConfirmAccountBody"], account.FirstName, code)
+            //});
+
+            await _context.SaveChangesAsync();
+
+            return new Result<Account>(account, "Account created successfully!");
+        }
+        catch (Exception ex)
+        {
+            return new Result<Account>(false, ex.ToString());
+        }
+    }
+
+    private bool IsUniqueUser(string email)
+    {
+        var user = _context.Accounts!.SingleOrDefault(x => x.Email == email);
+
+        if (user == null) return true;
+        return false;
     }
 }
