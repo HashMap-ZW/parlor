0a56c1a795dd1ec1c3ea034b53e4a4a6610bf846
Mgunara0305 <vuyelwamguni@gmail.com>
2024-09-05 18:09:21 +0200

    inventory completedd

---
+++ backend/Parlor.API/Controllers/InventoryController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Parlor.API.Dtos;
+using Parlor.API.Mappers;
+using Parlor.API.Models;
+using Parlor.API.Models.Repository.IRepository;
+
+namespace Parlor.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        private readonly IInventoryRepository _inventoryRepo;
+        public InventoryController(AppDbContext context, IInventoryRepository inventoryRepo)
+        {
+            _inventoryRepo = inventoryRepo;
+            _context = context; 
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetAll()
+        {
+            var inventories = await _inventoryRepo.GetAllAsync();
+            var inventoryDto = inventories.Select(s => s.ToInventoryDto());
+
+            return Ok(inventoryDto);  
+        }
+
+
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            var inventory = await _inventoryRepo.GetByIdAsync(id);
+
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(inventory.ToInventoryDto());
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create([FromBody] CreateInventoryRequestDto inventoryDto)
+        {
+            var inventoryModel = inventoryDto.ToInventoryFromCreateDTO();
+            await _inventoryRepo.CreateAsync(inventoryModel);
+
+            return Ok(inventoryModel);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateInventoryRequestDto updateDto)
+        {
+            var inventoryModel = await _inventoryRepo.UpdateAsync(id, updateDto);
+
+            if (inventoryModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(inventoryModel.ToInventoryDto());
+
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var inventoryModel = await _inventoryRepo.DeleteAsync(id);
+
+            if (inventoryModel == null)
+            {
+                return NotFound();
+            }
+      
+            return NoContent();
+        }
+
+
+
+
+    }
+}
--- backend/Parlor.API/Migrations/20240905094346_inventory migration.Designer.cs
+++ backend/Parlor.API/Migrations/20240905094346_inventory migration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Parlor.API.Models;
 
@@ -11,9 +12,11 @@ using Parlor.API.Models;
 namespace Parlor.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240905094346_inventory migration")]
+    partial class inventorymigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
